70ffc9379192aa9445a3a90aa61ccf68a5e7c513
Edgar Galleguillo <edgar.galleguillo@gmail.com>
2022-11-03 18:33:21 -0300

    change forms.py

--- consultora/forms.py
+++ consultora/forms.py
@@ -4,7 +4,7 @@ from .models import *
 class PostulanteCrearForm(forms.ModelForm):
 
     fechanacimiento = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label='Fecha de Nacimiento')
-    documento = forms.ChoiceField(queryset=Documento.objects.all())
+    documento = forms.ModelChoiceField(queryset=Documento.objects.all())
     class Meta:
         model = Postulante
         fields = '__all__'
